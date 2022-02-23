73359ca52aa1c9feb50104ee2d1b1f2c53587ec6
Grzegorz Wasik <wasikgrzegorz3@gmail.com>
2022-02-23 20:19:32 +0100

    Add NavBar and Footer

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,8 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+import About from './components/pages/About/About.js'
+import AddPost from './components/pages/AddPost/AddPost.js'
+import EditPost from './components/pages/EditPost/EditPost.js'
+import Error from './components/pages/Error/Error.js'
+import Main from './components/pages/Main/Main.js'
+import Post from './components/pages/Post/Post.js'
+import { Container } from 'react-bootstrap';
+import Header from './components/vievs/Header/Header.js'
+import Footer from './components/vievs//Footer/Footer.js'
+
 function App() {
   return (
-    <div>
-      <p>Hello world</p>
-    </div>
+    <Container>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/post/:id" element={<Post />} />
+        <Route path="/post/add" element={<AddPost />} />
+        <Route path="/post/edit/:id" element={<EditPost />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Container>
+
   );
 }
 
--- blog/src/index.js
+++ blog/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store.js'
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <BrowserRouter>
+          <Provider store={store}>
+              <App />
+          </Provider>
+      </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.querySelector('#root')
 );
